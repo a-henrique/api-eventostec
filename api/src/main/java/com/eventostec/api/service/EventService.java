6dd076c23aaafa36364a5f1e09a10390292abe24
a-henrique <audrinh@gmail.com>
2025-04-19 22:24:05 -0300

    AWS configuration, Event Controller define request

--- api/src/main/java/com/eventostec/api/service/EventService.java
+++ api/src/main/java/com/eventostec/api/service/EventService.java
@@ -1,14 +1,32 @@
 package com.eventostec.api.service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Autowired
+    private EventRepository eventRepository;
 
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
@@ -22,11 +40,31 @@ public class EventService {
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDataEvent(new Date(data.dataEvent()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
 
+        eventRepository.save(newEvent);
         return newEvent;
     }
 
     private String uploadImg(MultipartFile multipartFile){
-        return "";
+        String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+
+        try {
+            File file = this.convertMultipartToFile(multipartFile);
+            s3Client.putObject(bucketName, filename, file);
+            file.delete();
+            return s3Client.getUrl(bucketName, filename).toString();
+        } catch (Exception e){
+            System.out.println("erro ao subir arquivo");
+            return "";
+        }
+    }
+
+    private File convertMultipartToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
     }
 }
